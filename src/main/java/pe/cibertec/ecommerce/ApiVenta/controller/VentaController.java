01f31f40beb7d743f0429ad4b53ac73bc1d83dac
Stephano Gary <nafetsgary@outlook.es>
2023-11-27 01:55:51 -0500

    logs configurado

--- src/main/java/pe/cibertec/ecommerce/ApiVenta/controller/VentaController.java
+++ src/main/java/pe/cibertec/ecommerce/ApiVenta/controller/VentaController.java
@@ -1,6 +1,6 @@
-
 package pe.cibertec.ecommerce.ApiVenta.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +14,22 @@ import pe.cibertec.ecommerce.ApiVenta.service.VentaService;
 
 @RestController
 @RequestMapping("api/v1/venta")
-
+@Slf4j
 public class VentaController {
     
     @Autowired
     private VentaService ventaService;
+
     @PostMapping("/placeVenta")
-    public ResponseEntity<PurchaseResponse> placeOrder(@RequestBody Purchase purchase){
-        return new ResponseEntity<>(ventaService.placeVenta(purchase),HttpStatus.CREATED);
+    public ResponseEntity<PurchaseResponse> placeOrder(@RequestBody Purchase purchase) {
+        try {
+            log.info("Recibiendo solicitud para realizar una venta: {}", purchase);
+            PurchaseResponse response = ventaService.placeVenta(purchase);
+            log.info("Venta realizada exitosamente con número de seguimiento: {}", response.getTrackingNumber());
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } catch (Exception e) {
+            log.error("Error al procesar la solicitud de venta", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/pe/cibertec/ecommerce/ApiVenta/service/VentaServiceImpl.java
+++ src/main/java/pe/cibertec/ecommerce/ApiVenta/service/VentaServiceImpl.java
@@ -1,8 +1,8 @@
-
 package pe.cibertec.ecommerce.ApiVenta.service;
 
 import jakarta.transaction.Transactional;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pe.cibertec.ecommerce.ApiVenta.dao.VentaRepository;
@@ -11,6 +11,7 @@ import pe.cibertec.ecommerce.ApiVenta.dto.PurchaseResponse;
 import pe.cibertec.ecommerce.ApiVenta.entity.Venta;
 
 @Service
+@Slf4j
 public class VentaServiceImpl implements VentaService {
     
     @Autowired
@@ -19,15 +20,20 @@ public class VentaServiceImpl implements VentaService {
     @Override
     @Transactional
     public PurchaseResponse placeVenta(Purchase purchase) {
+        log.info("Procesando solicitud para realizar una venta: {}", purchase);
+
         Venta venta = purchase.getVenta();
         venta.setTrackingNumber(getTrackingNumber());
-        purchase.getDetalleVentas().forEach(i->{venta.addDetalleVenta(i);});        
+        purchase.getDetalleVentas().forEach(i -> venta.addDetalleVenta(i));
+        
+        log.info("Guardando información de la venta en la base de datos.");
         ventaRepository.save(venta);
+
+        log.info("Venta realizada exitosamente con número de seguimiento: {}", venta.getTrackingNumber());
         return new PurchaseResponse(venta.getTrackingNumber());
     }
 
     private String getTrackingNumber() {
         return UUID.randomUUID().toString();
     }
-    
 }
